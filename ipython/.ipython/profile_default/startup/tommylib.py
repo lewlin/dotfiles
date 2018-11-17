d3147b20466e4380b05b40ebdc0b4728cd7fcf91
Tommaso Biancalani <tbiancal@broadinstitute.org>
2018-11-17 15:30:00 -0500

    display_dfs

---
+++ ipython/.ipython/profile_default/startup/tommylib.py
@@ -0,0 +1,7 @@
+from IPython.display import display_html
+
+def display_dfs(*args):
+    html_str=''
+    for df in args:
+        html_str+=df.to_html()
+    display_html(html_str.replace('table','table style="display:inline"'),raw=True)
