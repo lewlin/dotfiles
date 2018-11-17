e99de915cbb287f55c6fe9972bf44695b76a2666
Tommaso Biancalani <tbiancal@broadinstitute.org>
2018-11-17 18:46:54 -0500

    various

--- ipython/.ipython/tommylib.py
+++ ipython/.ipython/tommylib.py
@@ -1,4 +1,16 @@
 from IPython.display import display_html
+from IPython.display import HTML, display
+
+
+def set_cell_background(color):
+    script = (
+        "var cell = this.closest('.jp-CodeCell');"
+        "var editor = cell.querySelector('.jp-Editor');"
+        "editor.style.background='{}';"
+        "this.parentNode.removeChild(this)"
+    ).format(color)
+
+    display(HTML('<img src onerror="{}">'.format(script)))
 
 def display_dfs(*args):
     html_str=''
