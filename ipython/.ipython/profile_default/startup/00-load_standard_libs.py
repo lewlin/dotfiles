25e44f9c13c20e96777fb68f33435b730278e2cf
Tommaso Biancalani <tbiancal@broadinstitute.org>
2019-01-26 15:54:12 -0500

    remove always used from startup libraries

--- ipython/.ipython/profile_default/startup/00-load_standard_libs.py
+++ ipython/.ipython/profile_default/startup/00-load_standard_libs.py
@@ -1,8 +1,8 @@
 # Always used
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import numpy as np
+#import pandas as pd
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 
 # Import tlib
 import os, sys
