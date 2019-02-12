5527bfc456edecc12c6a21e064db99f450b0460c
Tommaso Biancalani <tbiancal@broadinstitute.org>
2019-02-12 11:18:28 -0500

    update profile

--- ipython/.ipython/profile_default/startup/00-load_standard_libs.py
+++ ipython/.ipython/profile_default/startup/00-load_standard_libs.py
@@ -1,15 +1,17 @@
 # Always used
-#import numpy as np
-#import pandas as pd
-#import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 #import seaborn as sns
 
 # Import tlib
 import os, sys
 home = os.environ['HOME']
 git_dir = os.path.join(home, 'git')
+github_dir = os.path.join(home, 'github')
 sys.path.append(git_dir)  # find tommy lib
-import tlib.plot as tpl
-import tlib.notebook as tnb
+sys.path.append(github_dir)
+#import tlib.plot as tpl
+#import tlib.notebook as tnb
 #tpl.test()
 
