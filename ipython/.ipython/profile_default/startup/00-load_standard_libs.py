e940acb9f532a9dcecdfb3d8215fbe23ca7933e5
Tommaso Biancalani <tbiancal@broadinstitute.org>
2019-06-02 13:27:38 +0000

    disable mpl debug

--- ipython/.ipython/profile_default/startup/00-load_standard_libs.py
+++ ipython/.ipython/profile_default/startup/00-load_standard_libs.py
@@ -2,15 +2,20 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-#import seaborn as sns
-
-# Import tlib
+import logging
 import os, sys
+
+# set default paths
 home = os.environ['HOME']
 git_dir = os.path.join(home, 'git')
 forked_dir = os.path.join(home, 'forked')
 sys.path.append(git_dir)  # find tommy lib
 sys.path.append(forked_dir)  # find forked packages
+
+# dont display DEBUG matplotlib messages
+mpl_logger = logging.getLogger('matplotlib')
+mpl_logger.setLevel(logging.WARNING)
+
 #sys.path.append(github_dir)
 #import tlib.plot as tpl
 #import tlib.notebook as tnb
